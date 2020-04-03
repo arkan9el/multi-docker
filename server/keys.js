961ec9561df14e08a7865dbaf255b3a84075b469
Guillaume Ginet <gg@2cs.ch>
2020-04-03 17:10:09 +0200

    RAZ du repo

---
+++ server/keys.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Guillaume Ginet on 27.01.20
+ */
+
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
---
+++ worker/keys.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Guillaume Ginet on 27.01.20
+ */
+
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT
+};
